c5484d9f0bf0cde1b67331437c89a2731d43c383
xomachine <xomachiner@gmail.com>
2018-01-06 23:02:29 +0300

    Libs separation to make the structure cleaner in other utility building

--- signature_search/sigsearch.nim
+++ signature_search/sigsearch.nim
@@ -1,11 +1,11 @@
 from os import paramStr, paramCount
 from osproc import execProcess
+from tables import initTable, `[]`, `[]=`, contains, del, `$`
 from strutils import splitLines, parseHexInt, toHex, splitWhitespace, find,
                      isDigit, count, contains, split, strip
-from bfd import bfd_open, bfd_close, bfd_init, bfd_get_section_by_name,
+from libs.bfd import bfd_open, bfd_close, bfd_init, bfd_get_section_by_name,
                 getContent, bfd_count_sections, init, bfd_get_start_address
-from disasm import initDisasm, parseInstructions, Disasmer
-from tables import initTable, `[]`, `[]=`, contains, del, `$`
+from libs.disasm import initDisasm, parseInstructions, Disasmer
 
 proc addr2Pattern(address: Natural): string =
   var a: uint32 = address.uint32
@@ -27,7 +27,6 @@ proc parseEA(s: string): tuple[register: string, offset: int] =
 proc readMethod(disasmer: var Disasmer, madr: uint32): int =
   var registers = initTable[string, int]()
   registers["%esp"] = 0
-  var maxsp = 0
   for instr in parseInstructions(disasmer, madr):
     #echo instr.name, ": ", instr.src, " -> ", instr.dst
     case instr.name
@@ -46,7 +45,7 @@ proc readMethod(disasmer: var Disasmer, madr: uint32): int =
       if parsed.register in registers:
         let ea = registers[parsed.register] + parsed.offset
         registers[instr.dst] = ea
-        maxsp = max(maxsp, ea)
+        result = max(result, ea)
       elif instr.dst in registers:
         registers.del(instr.dst)
     of "mov", "movl":
@@ -64,9 +63,8 @@ proc readMethod(disasmer: var Disasmer, madr: uint32): int =
         if instr.src[^1] == ')':parseEA(instr.src)
         else:parseEA(instr.dst)
       if parsed.register in registers:
-        maxsp = max(maxsp, parsed.offset + registers[parsed.register])
-    maxsp = max(registers["%esp"], maxsp)
-  return max(4, maxsp)
+        result = max(result, parsed.offset + registers[parsed.register])
+    result = max(registers["%esp"], result)
 
 iterator methods(start: ptr char): uint32 =
   let reinterpreted = cast[ptr array[0, uint32]](start)
@@ -82,33 +80,35 @@ let filename = paramStr(1)
 let file = bfd_open(filename, nil)
 if not file.init():
   quit "Failed to open!"
-echo "Num of sections: ", file.bfd_count_sections()
-#let startAddr = bfd_get_start_address(file)
 let strSegment = file.bfd_get_section_by_name(".rodata")
 let textSegment = file.bfd_get_section_by_name(".text")
 let vtableSegment = file.bfd_get_section_by_name(".data.rel.ro")
 if strSegment.isNil or vtableSegment.isNil:
   discard file.bfd_close()
   quit "Failed to find section!"
-echo "strSegment size: ", strSegment.size
-echo "vtableSegment size: ", vtableSegment.size
 let textSegmentData = file.getContent(textSegment)
 let vtableSegmentData = file.getContent(vtableSegment)
 let strSegmentData = file.getContent(strSegment)
 var disasmer = file.initDisasm(textSegment, textSegmentData[0].unsafeAddr)
 const classPattern = "CAdapterSteam"
 var offset = -1
-echo "Searching for classPattern"
 while (offset = strSegmentData.find(classPattern, offset+1); offset >= 0):
   let realoffset = offset + strSegment.vma.int - 2
-  echo "Found at ", realoffset.toHex()
-  echo "Data: ", cast[cstring](strSegmentData[offset].unsafeAddr)
+  let classname = $cast[cstring](strSegmentData[offset].unsafeAddr)
+  stderr.writeLine(classname & " found at " & realoffset.toHex())
+  echo "!" & classname
   let strPattern = realoffset.addr2Pattern()
   let tinfo = vtableSegmentData.find(strPattern) + vtableSegment.vma.int - 4
-  echo "Found TInfo at: ", tinfo.toHex()
+  stderr.writeLine("Found TInfo at: " & tinfo.toHex())
   let vtPattern = tinfo.addr2Pattern()
   let vtable = vtableSegmentData.find(vtPattern) + 4
-  echo "Found VTable at: ", (vtable + vtableSegment.filepos.int).toHex()
+  stderr.writeLine("Found VTable at: " &
+                   (vtable + vtableSegment.filepos.int).toHex())
   for madr in methods(vtableSegmentData[vtable].unsafeAddr):
-    echo "Found method at: ", madr.toHex(), ", depth: ", readMethod(disasmer, madr)
+    let depth = readMethod(disasmer, madr)
+    stderr.writeLine("Method at: " & madr.toHex() &
+                     " dives into stack on depth: " & $depth)
+    echo $max(depth, 4)
+    # 4 is minimal depth because even an empty method receives its object as
+    # the first argument
 discard bfd_close(file)
