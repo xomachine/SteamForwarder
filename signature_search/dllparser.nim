ebe01ba0aee6d9d510d42b70ab627328a2e9803c
xomachine <xomachiner@gmail.com>
2018-01-11 01:53:11 +0300

    Added explicit information about libsteam_api.so location and arguments count to make linkage and code generation painless

--- signature_search/dllparser.nim
+++ signature_search/dllparser.nim
@@ -10,7 +10,12 @@ from libs.bfd import bfd_init, init, bfd_open, bfd_close, readSymbolTable,
                      BFS_GLOBAL, Symbol, BFD, bfd_get_start_address,
                      bfd_get_section_by_name, getContent
 
-proc findBestSOLib(verfolder: string, specdata: SpecFile): BFD =
+type
+  MatchingLib = tuple
+    path: string
+    handle: BFD
+
+proc findBestSOLib(verfolder: string, specdata: SpecFile): MatchingLib =
   var maxcount = 0
   for filename in walkFiles(verfolder & "/*/libsteam_api.so"):
     let bfile = bfd_open(filename, nil)
@@ -25,14 +30,14 @@ proc findBestSOLib(verfolder: string, specdata: SpecFile): BFD =
     if counter < maxcount:
       discard bfile.bfd_close()
     else:
-      if not result.isNil():
-        discard result.bfd_close()
+      if not result.handle.isNil():
+        discard result.handle.bfd_close()
       if specdata.len == counter:
         stderr.writeLine("Found exact match: " & filename)
-        return bfile
+        return (path: filename, handle: bfile)
       else:
         maxcount = counter
-        result = bfile
+        result = (path: filename, handle: bfile)
 
 
 assert(paramCount() == 1)
@@ -40,7 +45,7 @@ let specdata = stdin.readSpecFile()
 stderr.writeLine("Spec file: loaded " & $specdata.len & " entries")
 let versionfolder = paramStr(1)
 bfd_init()
-let bfile = findBestSOLib(versionfolder, specdata)
+let (libpath, bfile) = findBestSOLib(versionfolder, specdata)
 let startAddress = bfd_get_start_address(bfile)
 let symbols = bfile.readSymbolTable()
 let textSection = bfile.bfd_get_section_by_name(".text")
@@ -48,21 +53,23 @@ let textSectionData = bfile.getContent(textSection)
 var disasmer = initDisasm(bfile, textSection,
                           textSectionData[0].unsafeAddr)
 stderr.writeLine("Read " & $symbols.len & " symbols from library")
-var resolved = initTable[string, string]()
+var resolved = initTable[string, int]()
+echo "# ", libpath
 for sym in symbols:
   let name = $sym.name
   if sym.value != 0 and (sym.flags and BFS_GLOBAL) > 0 and
      name in specdata:
     let address = sym.value + startAddress
     let depth = disasmer.readProcedure(address)
-    let arguments = repeat("ptr", depth div 4).join(" ")
-    resolved[name] = arguments
+    resolved[name] = depth
     #echo name, ":", depth
 for entry in specdata.values:
   let tail =
     if entry.name in resolved:
-      "cdecl " & entry.name & "(" & resolved[entry.name] & ") " & entry.name &
-        "_"
+      let nargs = resolved[entry.name] div 4
+      let arguments = repeat("ptr", nargs).join(" ")
+      "cdecl " & entry.name & "(" & arguments & ") " & entry.name &
+        "_" & " #" & $nargs
     else:
       entry.callconv & " " & entry.name
   echo entry.idx, " ", tail
