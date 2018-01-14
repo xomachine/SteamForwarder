xomachine/SteamForwarder

commit 4212e5bc0a6657f7ded00b20b2440370b5bb63b2

Enhanced permission checking mechanism to avoid crashes

genmacros/wrapper.nim
@@ -7,9 +7,9 @@ from tables import initTable, `[]`, `[]=`, contains, values
 from wine import trace, moduleByAddress, ModInfo, checkAddr
 from classparser import readClasses
 from vtables import wrapClass, fastWrap
+from maps import getMMap, checkAddress, Flags
 
 let cls = readClasses()
-
 proc wrapIfClass(address: uint32, checker: proc(a: uint32): bool): uint32 =
   let vtableaddr = cast[ptr uint32](address)[]
   trace("Walking through the class...")
@@ -32,34 +32,31 @@ proc wrapIfClass(address: uint32, checker: proc(a: uint32): bool): uint32 =
   let descstr = $cast[cstring](strptraddr[]+2)
   trace("class name (%s)\n", strptraddr[])
   if descstr in cls:
-    wrapClass(descstr, address)
+    return wrapClass(descstr, address)
   else:
-#    trace("FIXME: %s is unknown signature! You should definetely report it!\n",
-#          strptraddr[])
-    address
+#    trace("Unknown class %s! This should be reported!\n", descstr[0].unsafeAddr)
+    return address
 
 proc wrapIfClass(address: uint32, m: Slice[uint32]): uint32 =
   proc checker(a: uint32): bool =
     a in m
   wrapIfClass(address, checker)
 
+
 proc wrapIfNecessary(address: uint32): uint32 =
   let already = fastWrap(address)
   if already > 0'u32:
     trace("Translating: %p -> %p\n", address, already)
     return already
-  if address > 0x10000'u32:
-    let m = moduleByAddress(address)
-    let endaddr = m.baseOfImage + m.imageSize
-    let name = cast[cstring](m.moduleName.unsafeAddr)
-    if name.len > 0:
-      trace("Found module %s at address %p in range %p-%p(%p)\n", name, address,
-            m.baseOfImage, endaddr, m.imageSize)
-    if name == "steamclient.so":
-      let slice = Slice[uint32](a: m.baseOfImage, b: endaddr)
-      return wrapIfClass(address, slice)
-    if checkAddr(address, 4):
-      proc checker(a: uint32): bool =
-        checkAddr(a, 4)
-      return wrapIfClass(address, checker)
+  let curMap = getMMap()
+  let affinity = curMap.checkAddress(address)
+  if Flags.read in affinity.permissions:
+    trace("Looks like %p - is valid address with %s at \"%s\"\n", address,
+          affinity.permissions.repr.cstring, affinity.name.cstring)
+    proc checker(a: uint32): bool =
+      let af = curMap.checkAddress(a)
+      if not (Flags.read in af.permissions and af.name == "steamclient.so"):
+        trace("invalid address permissions: %p %s (related to \"%s\")\n", a,
+              af.permissions.repr.cstring, af.name.cstring)
+    return wrapIfClass(address, checker)
   return address

genmacros/maps.nim
@@ -1,0 +1,86 @@
+
+from posix import Pid
+
+type
+  Flags* {.pure.} = enum
+    read
+    write
+    execute
+    private
+  ModuleEntry* = tuple
+    mrange: Slice[uint32]
+    permissions: set[Flags]
+    name: string
+  MemMaps* = seq[ModuleEntry]
+{.push cdecl.}
+proc getRealPid*(): Pid
+proc getMMap*(): MemMaps
+proc checkAddress*(m: MemMaps, address: uint32): ModuleEntry
+proc parseFlags(f: string): set[Flags]
+proc getCurWPath(): string
+proc getWPath(p: string): string
+{.pop.}
+
+from posix import getppid, Pid, getcwd, readlink
+from strutils import parseInt, format, strip, rsplit, split, parseHexInt
+from wine import trace
+
+proc getCurWPath(): string =
+  ## Obtains current executable working dir
+  var buffer = newString(256)
+  discard getcwd(buffer[0].addr, 255)
+  trace("ResultSetLen: %s\n", buffer.cstring)
+  $(buffer.cstring)
+
+proc getWPath(p: string): string =
+  ## Obtains current working dir of `p`
+  result = newString(256)
+  let read = readlink("/proc/" & $p & "/cwd", result[0].addr, 255)
+  result.setLen(read)
+
+proc parseFlags(f: string): set[Flags] =
+  if f[0] == 'r': result.incl(Flags.read)
+  if f[1] == 'w': result.incl(Flags.write)
+  if f[2] == 'x': result.incl(Flags.execute)
+  if f[3] == 'p': result.incl(Flags.private)
+
+proc getMMap(): MemMaps =
+  ## Obtains content of /proc/$$/maps and parses it to get real module mapping
+  ## The real mappings are needed to check correctness memory references
+  let realPid {.global.} = getRealPid()
+  let mapfile {.global.} = "/proc/" & $realPid & "/maps"
+  result = newSeq[ModuleEntry]()
+  for line in mapfile.lines():
+    let splitted = line.split()
+    if splitted.len < 3:
+      # illformed line, may be blank
+      continue
+    let bounds = splitted[0].split({'-'})
+    if bounds[0].len > 8:
+      # 64-bit address should be skipped while we are in 32-bit space
+      continue
+    let perms = parseFlags(splitted[1])
+    let name = splitted[^1].rsplit('/', 1)[^1]
+    let mrange = Slice[uint32](a: bounds[0].parseHexInt.uint32,
+                               b: bounds[1].parseHexInt.uint32)
+    result.add((mrange: mrange, permissions: perms, name: name))
+
+
+proc getRealPid(): posix.Pid =
+  ## Some workaround to get current PID in linux terms instead of windows PID
+  let ppid = getppid()
+  trace("PPID = %d\n", ppid)
+  let curPath = getCurWPath()
+  trace("WinExe = %s\n", curPath.cstring)
+  for line in "/proc/$1/task/$1/children".format(ppid).lines():
+    trace("Read line: %s\n", line.cstring)
+    let stripped = line.strip()
+    let pPath = getWPath(stripped)
+    trace("exeName: %s\n", pPath.cstring)
+    if curPath == pPath:
+      return stripped.parseInt().Pid
+
+proc checkAddress(m: MemMaps, address: uint32): ModuleEntry =
+  for entry in m:
+    if address in entry.mrange:
+      return entry

commit 7699ca2b2c8c2735ca27ee13bee2faa824d28f5d

Fixed checker bug introduced in 4212e5bc0a6657f7ded00b20b2440370b5bb63b2

genmacros/wrapper.nim
@@ -58,5 +58,7 @@ proc wrapIfNecessary(address: uint32): uint32 =
       if not (Flags.read in af.permissions and af.name == "steamclient.so"):
         trace("invalid address permissions: %p %s (related to \"%s\")\n", a,
               af.permissions.repr.cstring, af.name.cstring)
+        return false
+      return true
     return wrapIfClass(address, checker)
   return address