56bca8cd8b19656799e4bf834f4d8a622fa0bce5
xomachine <xomachiner@gmail.com>
2018-01-19 20:48:17 +0300

    Removed all files from old version and added missing from this one

--- config.h
+++
@@ -1,8 +0,0 @@
-
-#include "stdint.h"
-#define strncpy lstrcpynA
-#define __int64 long long
-#include "steam_api.h"
-#include "steam_gameserver.h"
-#include "wine/debug.h"
-WINE_DEFAULT_DEBUG_CHANNEL(steam_api);
--- debug.h
+++
@@ -1,5 +0,0 @@
-#include <stdarg.h>
-#include "wine/debug.h"
-
-WINE_DEFAULT_DEBUG_CHANNEL(steam_api);
-
