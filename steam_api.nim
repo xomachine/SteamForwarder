5aa7b0abc21f3cfeba3373f1229d5ee218210867
xomachine <xomachiner@gmail.com>
2018-01-20 00:11:32 +0300

    Escaping the paths passed to c linker

--- steam_api.nim
+++ steam_api.nim
@@ -3,7 +3,22 @@ from genmacros.wine import trace
 from genmacros.cdecls import generateLinuxDecls, generateWineDecls
 from genmacros.utils import parseFullSpec
 const specname {.strdefine.} = "steam_api.auto.spec"
-{.passL: specname.}
+{.passL: "'" & specname & "'".}
+
+
+#{.link: "callbacks.o".}
+#{.compile: "callbacks.cpp".}
+#{.compile: "steam_api.cpp".}
+#{.link: "steam_api.o".}
+#{.passL: "-lstdc++".}
+#{.passC: "-I.".}
+#{.passC: "-g".}
+#{.passC: "-Iautoclass".}
+#{.passC: "-Isteam".}
+#{.passC: "-fpermissive".}
+#{.passC: "-DVERSION_SAFE_STEAM_API_INTERFACES".}
+#{.passC: "-DUSE_BREAKPAD_HANDLER".}
+
 const spec = parseFullSpec(specname)
 generateLinuxDecls(spec)
 generateWineDecls(spec)
