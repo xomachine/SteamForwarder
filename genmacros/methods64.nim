5d69b0d6b5bdcb51015d414da51ffec203d9d6e5
xomachine <xomachiner@gmail.com>
2018-06-18 01:45:37 +0300

    Handling of CSteamID return value

--- genmacros/methods64.nim
+++ genmacros/methods64.nim
@@ -17,15 +17,18 @@ import macros
 
 static:
   var pmethods: set[uint8]
+  var pmethodsInMemory: set[uint8]
 
-proc makePseudoMethod(stack: uint8): NimNode {.compileTime.} =
-  result = newProc(newIdentNode(pseudoMethodPrefix & $stack))
-  let nargs = max(int(stack div 4), 0)
+proc makePseudoMethod(stack: uint8, hidden: bool): NimNode {.compileTime.} =
+  result = newProc(newIdentNode(pseudoMethodPrefix &
+                                (if hidden: "S" else: "") & $stack))
+  let nargs = max(int(stack div 4), 1 + int(hidden))
   let meaningfull_args = genArgs(nargs)
   result[3] = newTree(nnkFormalParams, meaningfull_args)
   let mcall = genAsmNativeCall64("origin_method", nargs)
   let omethod = newIdentNode("origin_method")
   let tracecall = genTraceCall(nargs)
+  let hdn = newLit(hidden)
   let procty = newTree(nnkProcTy, newTree(nnkFormalParams, meaningfull_args),
                        newTree(nnkPragma, newIdentNode("cdecl")))
   result.body = quote do:
@@ -47,12 +50,19 @@ proc makePseudoMethod(stack: uint8): NimNode {.compileTime.} =
     trace("Method address: %p\n", `omethod`)
     let res = `mcall`
     trace("Result = %p\n", res)
-    wrapIfNecessary(res)
+    when `hdn`:
+      let data = cast[ptr uint64](argument2)
+      data[] = res
+      cast[uint64](argument2)
+    else:
+      wrapIfNecessary(res)
 
 proc makePseudoMethods(): NimNode =
   result = newStmtList()
   for m in pmethods:
-    result.add(makePseudoMethod(m))
+    result.add(makePseudoMethod(m, false))
+  for m in pmethodsInMemory:
+    result.add(makePseudoMethod(m, true))
 
 proc makeMethodDesc(i: int, k: string, v: StackState): MethodDesc =
   var tq = newNimNode(nnkTripleStrLit)
@@ -60,7 +70,7 @@ proc makeMethodDesc(i: int, k: string, v: StackState): MethodDesc =
   # pop %rbp # necessary because compiler ignores noAsmStackFrame somehow
   # xchg %rcx, %rdx # it looks like wine already performs call conversion
   mov $0x""" & i.toHex & """, %r11 # The virtual method number
-  jmp `""" & pseudoMethodPrefix & $v.depth & """`
+  jmp `""" & pseudoMethodPrefix & (if v.swap: "S" else: "") & $v.depth & """`
 """
   result.body = newTree(nnkAsmStmt, newEmptyNode(), tq)
   result.name = "m" & k & $i
@@ -69,7 +79,10 @@ proc makeVTableDesc(name: string, sstates: seq[StackState]): VTableDesc =
   result.name = name
   result.methods = newSeq[MethodDesc]()
   for i, v in sstates.pairs():
-    pmethods.incl(v.depth.uint8)
+    if v.swap:
+      pmethodsInMemory.incl(v.depth.uint8)
+    else:
+      pmethods.incl(v.depth.uint8)
     result.methods.add(makeMethodDesc(i, name, v))
 
 proc makeAPIDesc*(classes: Classes): APIDesc =
